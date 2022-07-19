bc4fddb3d346a85fe46484dcfb0b890cd4222764
valentine8447 <99423767+valentine8447@users.noreply.github.com>
2022-07-19 14:32:30 -0700

    comitce

---
+++ Chapter02/Chapter02/Numbers/Program.cs
@@ -0,0 +1,90 @@
+﻿// unsigned integer means positive whole number or 0
+uint naturalNumber = 23;
+
+
+// integer means negative or positive whole number or 0
+int integerNumber = -23;
+// float means single-precision floating point
+// F suffix makes it a float literal
+float realNumber = 2.3F;
+
+// double means double-precision floating point
+double anotherRealNumber = 2.3;// double literal
+
+Console.WriteLine(naturalNumber);
+Console.WriteLine(integerNumber);
+Console.WriteLine(realNumber);
+Console.WriteLine(anotherRealNumber);
+
+// three variables that store the number 2 million
+//int decimalNotation = 2_000_000;
+//int binaryNotation = 0b_0001_ 1110_1000_0100_1000_0000;
+//int hexaDecimalNotation = 0x_001E_8480;
+
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexaDecimalNotation = 0x_001E_8480;
+
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine($"{decimalNotation == hexaDecimalNotation}");
+
+Console.WriteLine(decimalNotation);
+Console.WriteLine(binaryNotation);  
+Console.WriteLine(hexaDecimalNotation);
+//Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in
+//the range { int .MinValue:N0}
+//to
+//{ int.MaxValue:N0}. ");
+//Console.WriteLine($"double uses {sizeof(double)} bytes and can store"
+//numbers in the range {double.Min.Value:N0}
+//Console.WriteLine($"decimal uses  {sizeof(decimal)} bytes and can store
+//numbers in the range { decimal.MinValue:N0}
+//to
+//{decimal.MaxValue:N0}. ");
+
+Console.WriteLine($"in uses {sizeof(int)} bytes " +
+    $"and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}");
+
+
+Console.WriteLine($"in uses {sizeof(double)} bytes " +
+    $"and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}");
+
+Console.WriteLine($"in uses {sizeof(decimal)} bytes " +
+    $"and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}");
+
+
+Console.WriteLine("Using doubles:");
+double a = 0.1;
+double b = 0.2;
+
+if (a + b == 0.3)
+{
+    Console.WriteLine($"{a} + {b} equals {0.3}");
+
+}
+else
+{
+    Console.WriteLine($"{a} + {b} does NOT equal {0.3}");
+
+
+
+
+
+
+    Console.WriteLine("Using decimal:");
+    decimal c = 0.1M;
+    decimal d = 0.2M;
+
+    if (c + d == 0.3M)
+    {
+        Console.WriteLine($"{c} + {d} equals {0.3}");
+
+    }
+    else
+    {
+        Console.WriteLine($"{c} + {d} does NOT equal {0.3}");
+    }
+}
+
+bool isBreak = false;
+Console.WriteLine(isBreak);
---
+++ Chapter02/Chapter02/Vocabulary/Program.cs
@@ -0,0 +1,81 @@
+﻿
+
+//#error version
+using System.Reflection;
+
+
+
+/*System.Data.DataSet ds;
+HttpClient client;
+List<string> list;*/
+
+
+
+
+Assembly? Assembly = Assembly.GetEntryAssembly();
+
+// loop through the assemblies that this app reference
+foreach (AssemblyName name in Assembly.GetReferencedAssemblies())
+{
+    // Load the assembly so we can read its details
+    Assembly a = Assembly.Load(name);
+
+    // declare a variable to count the number of methods 
+    int methodCount = 0;
+
+    // Loop through all the types in the assembly
+    foreach (TypeInfo t in a.DefinedTypes)
+    {
+        // add up the counts of methods
+        methodCount += t.GetMethods().Count();
+    }
+
+    // output the count of types and their methods
+    Console.WriteLine(
+        "{0:N0} types with {1:N0} methods in {2} assambly.",
+        arg0: a.DefinedTypes.Count(),
+        arg1: methodCount, arg2: name.Name);
+     
+}
+//let the heightInMetres variable become equal to the value 1.88
+double heightInMetres = 1.88;
+Console.WriteLine($"The variable {heightInMetres.GetType()} has the value {1.88}");
+
+//For text, a single letter, such as an A, is stored as a char type.
+//char letter = 'A';
+//char digit = '1';
+//char symbol = '$';
+//char userChoice = GetSomeKeystroke();//assigning from a fuctitous function
+
+char letter = 'A';
+char digit = '1';
+char symbol = '$';
+char userChoice = Console.ReadKey().KeyChar;
+
+Console.WriteLine(
+    $"{nameof(letter)} : {letter} /r/n " +
+    $"{nameof(digit)} : {digit} /r/n " +
+    $"{nameof(symbol)} : {symbol} /r/n " +
+    $"{nameof(userChoice)} : {userChoice}");
+
+//For text, multiple letters, such as Bob, are stored as a string type and are assigned using double quotes around the literal value
+string firstName = "Bob";
+string lastName = "Smith";
+string phoneNumber = "(215) 555-4256";
+Console.WriteLine(
+    $"{nameof(firstName)} : {firstName} /r/n " + 
+    $"{nameof(lastName)}: {lastName} /r/n "  +
+    $"{nameof(phoneNumber)} : {phoneNumber}");
+
+
+
+
+string fullNameWithTabSeparator = "Bob\tSmith";
+
+Console.WriteLine(fullNameWithTabSeparator);
+
+
+string filePath = "C:\\televisions\\sony\\bravia.txt";
+string filePath2 = @"C:\televisions\sony\bravia.txt";
+
+Console.WriteLine($"{filePath} \r\n {filePath2}");   
